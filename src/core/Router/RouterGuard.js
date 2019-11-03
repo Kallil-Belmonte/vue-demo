06c4ed02eae7d72e4ac55c95e344e7e757d48e38
Kallil <kallil.belmonte@gmail.com>
2019-11-03 18:15:27 -0300

    Files updated.

--- src/core/Router/RouterGuard.js
+++ src/core/Router/RouterGuard.js
@@ -5,12 +5,10 @@ const RouterGuard = (to, from, next) => {
   if (authToken && !expiredSession) {
     next();
   } else {
-    // Remove token
     sessionStorage.removeItem('authTokenVueDemo');
     localStorage.removeItem('authTokenVueDemo');
     localStorage.removeItem('expirationDateVueDemo');
 
-    // Redirect
     next({ name: 'login' })
   }
 };
