29f6b42e85fa28c5807d9779433c752ac35dd2d9
Kallil <kallil.belmonte@gmail.com>
2021-10-19 06:55:49 -0300

    File updated.

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -1,5 +1,5 @@
-export { default as AppIcon } from './AppIcon/AppIcon.vue';
 export { default as AppAlertDismissible } from './AppAlertDismissible.vue';
+export { default as AppIcon } from './AppIcon/AppIcon.vue';
 export { default as AppLoader } from './AppLoader.vue';
 export { default as AppPageHeader } from './AppPageHeader.vue';
 export { default as AppPostItem } from './AppPostItem.vue';
