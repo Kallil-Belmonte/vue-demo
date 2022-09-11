add266a2ce5c05647eb791f861efb234044ddc1b
Kallil Belmonte <kallil.belmonte@gmail.com>
2022-09-11 09:36:07 -0300

    Files updated.

--- src/core/router/index.ts
+++ src/core/router/index.ts
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import guard from './guard';
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.VITE_BASE_URL),
   routes: [
     {
       path: '/',
