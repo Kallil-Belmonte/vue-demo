28c56262970c4b7b08d2dc565c5fe2c08d2d5ef5
Kallil Belmonte <kallil.belmonte@gmail.com>
2022-09-21 06:37:39 -0300

    Files updated.

--- src/shared/helpers/form/getFieldClass.ts
+++ src/shared/helpers/form/getFieldClass.ts
@@ -9,7 +9,7 @@ const getFieldClass = (
   field: UseField,
   baseClassNames = ['form-control'],
 ) => {
-  const classes = baseClassNames;
+  const classes = [...baseClassNames];
   if ((isFormSubmitted || field.state.dirty) && field.state.invalid) classes.push('is-invalid');
   return classes;
 };
