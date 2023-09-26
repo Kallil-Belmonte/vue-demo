fa2d04ec08900a8ce85208da98e5c98081aaed01
Kallil Belmonte <kallil.belmonte@gmail.com>
2023-09-26 15:38:45 -0300

    File updated.

--- src/shared/files/validations.ts
+++ src/shared/files/validations.ts
@@ -1,13 +1,16 @@
 import { Ref } from 'vue';
 
-import { Config } from '@/shared/helpers/string/validate';
+import { ValidationConfig } from '@/shared/helpers/string/validate';
 
-export const required: Config = { required: { check: true } };
+export const required: ValidationConfig = { required: { check: true } };
 
-export const requiredEmail: Config = { ...required, email: { check: true } };
+export const requiredEmail: ValidationConfig = { ...required, email: { check: true } };
 
 export const requiredSelect = (model: Ref<string>) => ({
   custom: { isValid: model.value !== 'select', message: 'Required value.' },
 });
 
-export const requiredMin = (min: number): Config => ({ ...required, min: { check: min } });
+export const requiredMin = (min: number): ValidationConfig => ({
+  ...required,
+  min: { check: min },
+});
