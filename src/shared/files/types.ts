34ceec738b45940068987a166ad91bfbfd162c14
Kallil Belmonte <kallil.belmonte@gmail.com>
2024-09-22 14:10:51 -0300

    Deleted type.

--- src/shared/files/types.ts
+++ src/shared/files/types.ts
@@ -6,8 +6,6 @@ export type ButtonType = ButtonHTMLAttributes['type'];
 
 export type ObjectType = { [key: string]: any };
 
-export type UserFrom = 'user' | 'search' | 'match' | 'match-to-choose';
-
 export type Variant = 'primary' | 'secondary' | 'base';
 
 export type Status = 'info' | 'success' | 'warning' | 'danger';
