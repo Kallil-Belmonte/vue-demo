ee2c20679a0f168e946c5bff53e8695c2ada29d5
Kallil Belmonte <kallil.belmonte@proton.me>
2025-12-06 17:03:37 -0300

    Files updated.

--- src/core/services/_files/endpoints.ts
+++ src/core/services/_files/endpoints.ts
@@ -1,5 +1,3 @@
-const getEnv = (env: string | boolean | undefined) => (typeof env === 'string' ? env : '');
+export const MOCKY_API = 'https://run.mocky.io/v3';
 
-export const MOCKY_API = getEnv(import.meta.env.VITE_MOCKY_URL);
-
-export const JSON_PLACEHOLDER_API = getEnv(import.meta.env.VITE_JSON_PLACEHOLDER_URL);
+export const JSON_PLACEHOLDER_API = 'https://jsonplaceholder.typicode.com';
